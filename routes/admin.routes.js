9dc48c6f15a64c3464ce4c9811e95030af5e53a1
Sinan Koyuncu <koyuncusinan1@gmail.com>
2022-11-28 13:06:12 +0300

    Insert func of adding and fetching product using db

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
+
 const adminController = require("../controllers/admin.controller")
+const imageUploadMiddleware = require("../middlewares/image-upload");
 
 router.get("/products",adminController.getProducts)
 
 
 router.get("/products/new", adminController.getNewProduct);
+
+router.post("/products", imageUploadMiddleware,adminController.createNewProduct)
 
 
 
